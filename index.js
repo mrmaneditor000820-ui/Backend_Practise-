27616bdc27d69f2ff3684f433181229ba5ddddfa
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-07-15 21:30:37 +0500

    Initialize Express server and update package.json for module support

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-ege
+import express from "express"
+
+const app = express();
+
+const PORT = 3000;
+
+app.get("/" , (req,res)=>{
+    res.send ("hello backend");
+})
+
+app.listen(PORT ,()=>{
+   console.log(`server is running on port ${PORT}`);
+   
+   
+})
