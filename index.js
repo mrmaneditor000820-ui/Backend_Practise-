5ef0997e05a4def8b66e99b055e36fb1e79d71d6
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-07-16 08:37:46 +0500

    env file setup

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 import express from "express"
-
+import dotenv from 'dotenv'
+dotenv.config();
 const app = express();
 
-const PORT = 3000;
+const port = process.env.PORT 
 
 app.get("/" , (req,res)=>{
     res.send ("hello backend");
 })
 
-app.listen(PORT ,()=>{
-   console.log(`server is running on port ${PORT}`);
+app.listen(port ,()=>{              //app.listen ye pore app ko dekhe ga koi chiz a rai hy ya etc.
+   console.log(`server is running on port ${port}`);
    
    
 })
