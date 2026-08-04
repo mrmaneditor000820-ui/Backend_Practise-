4685e4dab3ad201cd714aa4b0a7be54f2806a13f
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-08-04 07:19:15 +0500

    patch api testing

--- Backend 2/app.js
+++ Backend 2/app.js
@@ -7,26 +7,37 @@ const app = express()
 app.use(express.json())
 
 const notes = []
-app.post('/notes',(req,res)=>{
+app.post('/notes', (req, res) => {
     notes.push(req.body)
     res.status(201).json({
-        message:"notes created successfull"
+        message: "notes created successfull"
     })
 })
 
-app.get('/notes',(req,res)=>{
+app.get('/notes', (req, res) => {
     res.status(200).json({
-        message:"notes fetched successfully",
-        notes:notes
+        message: "notes fetched successfully",
+        notes: notes
     })
 })
 
-app.delete('/notes/:index',(req,res)=>{
-     
+app.delete('/notes/:index', (req, res) => {
+
     const index = req.params.index
-    delete notes [index]
-    res.status(200).json ({
-        message:"note deleted succussesfully"
+    delete notes[index]
+    res.status(200).json({
+        message: "note deleted succussesfully"
+    })
+})
+
+app.patch("/notes/:index", (req, res) => {
+
+    const index = req.params.index
+    const discription = req.body.discription
+    notes[index].discription = discription
+
+    res.status(200).json({
+        message: "notes updated succssfully"
     })
 })
 
